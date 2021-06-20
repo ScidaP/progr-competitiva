0578020fbfd6fb24eceb1823a0c0917b430630b7
Patricio Scidá <patricioscida@gmail.com>
2021-06-20 20:47:17 -0300

    Terminado

---
+++ VISA.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        int x1, x2, y1, y2, z1, z2;
+        scanf("%d %d %d %d %d %d", &x1, &x2, &y1, &y2, &z1, &z2);
+        if (x2 >= x1 && y2 >= y1 && z2 <= z1) {
+            puts("YES");
+        } else {
+            puts("NO");
+        }
+    }
+    return 0;
+}
