0d58294aeae1a38bd55701db6d51a9f3e707c748
Patricio Scidá <patricioscida@gmail.com>
2021-07-11 20:27:27 -0300

    Terminado

--- WATSCORE.c
+++ WATSCORE.c
@@ -11,12 +11,17 @@ int main() {
         for (int i = 0; i < cant*2; i+=2) {
             scanf("%d %d", &casos[i], &casos[i+1]);
         }
-        for (int j = 0; j < cant*2; j+=2) {
-            if (j <= 8) {
-                suma += casos[j+1];
+        int aux = 0;
+        for (int j = 1; j <= 8; j++) {
+            for (int k = 0; k < cant*2; k+=2){
+                if (casos[k] == j && casos[k] <= 8 && casos[k+1] > aux) {
+                    aux = casos[k+1];
+                }
             }
+            suma += aux;
+            aux = 0;
         }
-        printf("%d", suma);
+        printf("%d\n", suma);
     }
     getchar();
     getchar();
