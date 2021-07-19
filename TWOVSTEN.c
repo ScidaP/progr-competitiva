13b56a8cd874546a26c94f07e7daf3e994ebb2c9
Patricio Scidá <patricioscida@gmail.com>
2021-07-19 19:23:12 -0300

    Terminado

--- TWOVSTEN.c
+++ TWOVSTEN.c
@@ -4,20 +4,14 @@ int main() {
     int t;
     scanf("%d", &t);
     while (t--) {
-        int n, cont = 0; // cambiar n por long long o algo así.
+        int n;
         scanf("%d", &n);
-        if (n % 10 != 0) {
-            while (n % 10 != 0 || cont >= 100){
-                n *= 2;
-                cont++;
-            }
-            if (n % 10 == 0) {
-                printf("%d\n", cont);
-            } else {
-                puts("-1");
-            }
+        if (n % 10 == 0) {
+            printf("0");
+        } else if (n % 5 == 0 && n % 10 != 0) {
+            printf("1");
         } else {
-            puts("0");
+            printf("-1");
         }
     }
     return 0;
